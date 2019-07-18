7829c5baecefe0b02826ce5fcd0dbbd83a3878e9
smessiah777 <smessiah17@gmail.com>
2019-07-18 12:43:56 -0500

    refactored index.js class App code into simpler state intialization without conscructor function

--- client/src/index.js
+++ client/src/index.js
@@ -4,21 +4,20 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
 
-    constructor(props) {
-            super(props);
+    
 
-            this.state = { lat: null, 
-                           errorMessage: "" 
-                        };
+    state = { lat: null, 
+        errorMessage: "" 
+     };
+    
 
-            window.navigator.geolocation.getCurrentPosition(
-                position => {
-                        this.setState({ lat: position.coords.latitude});
-                },
-                err => {
-                    this.setState({ errorMessage: err.message});
-                }
-            );
+    componentDidMount(){
+        window.navigator.geolocation.getCurrentPosition(
+            position => 
+                    this.setState({ lat: position.coords.latitude}),
+            err => this.setState({ errorMessage: err.message})
+            
+        );
     }
     
     render() {
