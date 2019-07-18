c479ac343f7ac225e67a31e857da27791f5bc782
smessiah777 <smessiah17@gmail.com>
2019-07-18 00:02:35 -0500

    initialized state and set state to update if latitude data was obtained and if not update with erro message

--- client/src/index.js
+++ client/src/index.js
@@ -2,17 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-
-
 class App extends React.Component {
+
+    constructor(props) {
+            super(props);
+
+            this.state = { lat: null, 
+                           errorMessage: "" 
+                        };
+
+            window.navigator.geolocation.getCurrentPosition(
+                position => {
+                        this.setState({ lat: position.coords.latitude});
+                },
+                err => {
+                    this.setState({ errorMessage: err.message});
+                }
+            );
+    }
     
     render() {
-        window.navigator.geolocation.getCurrentPosition(
-            position => console.log(position),
-            err => console.log(err)
-            
-        );
-        return <div>Latitude:</div>
+        
+        
+            if (this.state.errorMessage && !this.state.lat) {
+                return <div>Error: {this.state.errorMessage}</div>
+            }
+            if (!this.state.errorMessage && this.state.lat) {
+                return <div>Latitude: {this.state.lat}</div>
+            }
+           return <div>Loading!</div>
     }
 }
 
